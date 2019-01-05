f129b4280d85047b547767c6e98324f855e52d8d
Vince Carey <stvjc@channing.harvard.edu>
2019-01-05 16:00:27 -0500

    basic improvements, all signatures harvested, message on superclasses

--- R/defdMethods.R
+++ R/defdMethods.R
@@ -1,18 +1,35 @@
 #' defdMethods enumerates methods defined for a class in a package -- NEEDS WORK
 #' @param cl character(1) name of class
 #' @param pkg character(1) name of package where class is defined, defaults to 'package:[classname]' which works for major classes like SummarizedExperiment
-#' @note This is an early attack on the problem of enumerating
-#' defined methods for a class.  As of 5 Jan 2019 this function
-#' lists only one signature per method and fails on oddly
-#' names signature components like '_data'.
-#' @value an S4Vectors::DataFrame instance with columns 'mname' for method name, and 'sigs', a CharacterList of named signature elements (each element is a character vector of method parameter types, named with parameter names)
+#' @param verbose logical(1) if TRUE (default) will issue a message enumerating superclasses.
+#' @note Users should consider how methods inherited from superclasses defined in
+#' other packages are also relevant to reports of this type.  See the example.
+#' @value a data.frame instance with columns 'method', 'cl', 'pkg',
+#' 'nmeth4cl', counting the number of method-signature combinations for
+#' this method, and 'sigs', a list of lists of named signature elements (each element is a list of character vectors of method parameter types, named with parameter names)
 #' @examples
+#' # start with simple tabulation of package-specific methods
+#' suppressPackageStartupMessages({
 #' require("SingleCellExperiment")
+#' })
 #' scem = defdMethods("SingleCellExperiment")
-#' scem
-#' scem[[2]][[1]]
+#' head(scem)
+#' scem$method
+#' scem["isSpike", "sigs"]
+#' # now let's collect some related classes, with the objective of
+#' # enumerating relevant coercion methods and their signatures
+#' require("SummarizedExperiment")
+#' se = defdMethods("SummarizedExperiment", verbose=FALSE)
+#' rse = defdMethods("RangedSummarizedExperiment", "package:SummarizedExperiment", verbose=FALSE)
+#' cmb = try(rbind(scem,se,rse))
+#' require("dplyr")
+#' require("magrittr")
+#' tmp = (cmb %>% filter(method=="coerce"))
+#' sl = (tmp %>% select(sigs))[[1]]
+#' names(sl) = tmp$cl
+#' sl
 #' @export
-defdMethods = function(cl, pkg=paste0("package:", cl)) {
+defdMethods = function(cl, pkg=paste0("package:", cl), verbose=TRUE) {
  stopifnot(pkg %in% search())
  ## define helpers
  cleanF = function(st) {
@@ -21,21 +38,29 @@ defdMethods = function(cl, pkg=paste0("package:", cl)) {
  getSig = function(st) {
    eval(parse(text=paste0("c(", st, ")")))
    }
- ranstr = function(x) paste0(sample(letters,5), collapse="")
+ #ranstr = function() paste0(sample(letters,5), collapse="")
  ## end helpers
- #zz = ranstr()
  z = textConnection("zz", open="w", local=TRUE) # sets up local variable zz
  showMethods(class=cl, where=pkg, printTo=z) # gets class metadata as defined in package
  nminds = grep("Function", zz) # defined function indices
  mnames = cleanF(zz[nminds]) # defined method(function) names
-#
-# FIXME: this assumes one signature per method, completely wrong
-# Need to find number of sigs per method and operate accordingly
-#
- sigs = lapply(zz[nminds+1], function(x) try(getSig(x))) # defined signatures
- #list(mnames, sigs)
- message("there are probably more signatures per method than listed here")
- message("see FIXME in source")
- S4Vectors::DataFrame(mnames=mnames, sigs=S4Vectors::SimpleList(sigs))
+ smt = methods:::.showMethodsTable
+ allsigs = lapply(mnames, function(x) {
+       z = tempfile(); 
+       con = file(z, "w")
+       smt(getGeneric(x), classes=cl, printTo=con)
+       on.exit({close(con); try(unlink(z))}); 
+       ans = readLines(z)
+       ans[-c(1,length(ans))]
+      })
+ ans = data.frame(method=mnames, cl=cl, pkg=pkg, nmeth4cl=sapply(allsigs,length), sigs=mnames,
+   stringsAsFactors=FALSE)
+ ans$sigs = allsigs
+ supcl = names(getClass(cl)@contains)
+ if (length(supcl)>0 & isTRUE(verbose)) {
+    sups = paste0(supcl, collapse=", ")
+    message(paste("superclasses", sups, "may provide additional relevant methods/signatures"))
+    }
+ ans
 }
 
